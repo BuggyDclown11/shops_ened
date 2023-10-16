3271bc05fd3914aad4156fce39932b98ea782482
BuggyDclown11 <100565790+BuggyDclown11@users.noreply.github.com>
2023-10-16 09:22:27 +0545

    changes

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const morgan = require("morgan");
+const app = express();
+const mongoose = require("mongoose");
+const port = 5000;
+const userRoutes = require("./routes/userRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const cors = require("cors");
+const fileUpload = require("express-fileupload");
+
+mongoose.set("strictQuery", false);
+
+mongoose
+  .connect(
+    "mongodb+srv://shresthasijal9:Oj3XibEScAncAYFT@cluster0.dev9irq.mongodb.net/Shops"
+  )
+  .then((result) => {
+    app.listen(port);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
+
+app.use(
+  fileUpload({
+    limits: { fileSize: 50 * 1024 * 1024 },
+    abortOnLimit: true,
+    createParentPath: true,
+  })
+);
+
+app.use(userRoutes);
+app.use(productRoutes);
+app.use(orderRoutes);
